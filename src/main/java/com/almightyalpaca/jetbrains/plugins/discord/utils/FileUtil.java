0a09dd236a18e436766972f10aea3f0490fc62a8
Almighty Alpaca <aljoschagrebe@gmail.com>
2018-10-10 22:27:11 +0200

    Fix not closing file

--- src/main/java/com/almightyalpaca/jetbrains/plugins/discord/utils/FileUtil.java
+++ src/main/java/com/almightyalpaca/jetbrains/plugins/discord/utils/FileUtil.java
@@ -5,6 +5,7 @@ import com.intellij.notification.Notifications;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -12,17 +13,16 @@ import java.nio.file.Paths;
 
 public class FileUtil
 {
-
     @NotNull
     public static String readFirstLine(@NotNull VirtualFile file)
     {
-        try
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(file.getPath()), StandardCharsets.UTF_8))
         {
-            String line = Files.newBufferedReader(Paths.get(file.getPath()), StandardCharsets.UTF_8).readLine();
+            String line = reader.readLine();
 
             if (line != null)
-                if (line.length() > 512)
-                    return line.substring(0, 512);
+                if (line.length() > 256)
+                    return line.substring(0, 256);
                 else
                     return line;
         }
