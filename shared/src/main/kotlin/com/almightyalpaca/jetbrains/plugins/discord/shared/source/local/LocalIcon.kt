a388dda2533d47e0304e5c4c54780812111a10ec
Almighty Alpaca <aljoschagrebe@gmail.com>
2020-05-03 21:27:40 +0200

    Update copyright header

--- shared/src/main/kotlin/com/almightyalpaca/jetbrains/plugins/discord/shared/source/local/LocalIcon.kt
+++ shared/src/main/kotlin/com/almightyalpaca/jetbrains/plugins/discord/shared/source/local/LocalIcon.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 Aljoscha Grebe
+ * Copyright 2017-2020 Aljoscha Grebe
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
