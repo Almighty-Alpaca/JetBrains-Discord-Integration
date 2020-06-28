b58cf7e47f5c4c16f5289a40792d9d6afeb8d302
infrandomness <infrandomness@gmail.com>
2020-06-28 21:30:23 +0200

    Add tests

--- analytics/server/src/test/kotlin/com/almightyalpaca/jetbrains/plugins/discord/analytics/server/ApplicationTest.kt
+++ analytics/server/src/test/kotlin/com/almightyalpaca/jetbrains/plugins/discord/analytics/server/ApplicationTest.kt
@@ -16,17 +16,27 @@
 
 package com.almightyalpaca.jetbrains.plugins.discord.analytics.server
 
+import com.almightyalpaca.jetbrains.plugins.discord.analytics.model.Analytics
+import com.almightyalpaca.jetbrains.plugins.discord.analytics.server.database.generated.Tables.FILE_STATS
 import com.almightyalpaca.jetbrains.plugins.discord.analytics.server.routes.main
 import com.almightyalpaca.jetbrains.plugins.discord.analytics.server.services.Configuration
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.setBody
 import io.ktor.server.testing.withTestApplication
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonConfiguration
+import org.jooq.impl.DSL
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.images.PullPolicy
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
+import java.time.OffsetDateTime
 
 class KPostgreSQLContainer(dockerImageName: String?) : PostgreSQLContainer<KPostgreSQLContainer>(dockerImageName)
 
@@ -37,30 +47,81 @@ class ApplicationTest {
     private val databasePassword = "12345678"
     private val databaseName = "analytics"
 
+    private val token = "abcdefghijklmnopqrstuvwxyz"
+
     @Container
     val postgres: KPostgreSQLContainer = KPostgreSQLContainer(databaseImage)
         .withUsername(databaseUser)
         .withPassword(databasePassword)
         .withDatabaseName(databaseName)
+        .withImagePullPolicy(PullPolicy.alwaysPull())
 
-    val config: Configuration
+    private val databaseUrl
+        get() = "jdbc:pgsql://${postgres.host}:${postgres.firstMappedPort}/$databaseName"
+
+    private val config: Configuration
         get() {
-            val databaseUrl = "jdbc:pgsql://${postgres.host}:${postgres.firstMappedPort}/$databaseName"
-
             val database = Configuration.Database(databaseUrl, databaseUser, databasePassword)
 
             val deployment = Configuration.Ktor.Deployment(null, 8080, null, null, null)
 
             val ktor = Configuration.Ktor(null, deployment, null)
 
-            return Configuration(database, ktor, null)
+            val authentication = Configuration.Authentication(setOf(token))
+
+            return Configuration(database, ktor, authentication)
         }
 
+    private val testObject = Analytics(
+        listOf(
+            Analytics.File(
+                OffsetDateTime.now(),
+                "IntelliJ Idea",
+                "kotlin",
+                ".kt",
+                "kotlin"
+            )
+        ),
+        listOf(
+            Analytics.Icon(
+                OffsetDateTime.now(),
+                ".http",
+                "Atom",
+                "IntelliJ Idea",
+                "http",
+                "fallback"
+            )
+        ),
+        Analytics.Version(
+            OffsetDateTime.now(),
+            "2020.2.1",
+            "IJ"
+        )
+    )
+
     @Test
-    fun testRoot(): Unit = withTestApplication({ main(config) }) {
+    fun `correct test endpoint response`(): Unit = withTestApplication({ main(config) }) {
         with(handleRequest(HttpMethod.Get, "/test")) {
             assertEquals(HttpStatusCode.OK, response.status())
             assertEquals("Hello World!", response.content)
         }
+    }
+
+    @Test
+    fun `receive and store valid analytics data`(): Unit = withTestApplication({ main(config) }) {
+        val json = Json(JsonConfiguration.Stable)
+
+        val call = handleRequest(HttpMethod.Post, "api/v1/analytics") {
+            addHeader(HttpHeaders.Authorization, token)
+            addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+            setBody(json.stringify(Analytics.serializer(), testObject))
+        }
+
+        with(call) {
+            assertEquals(HttpStatusCode.OK, response.status())
+        }
+
+        val dsl = DSL.using(databaseUrl, databaseUser, databasePassword)
+        println(dsl.select(DSL.asterisk()).from(FILE_STATS).fetch())
     }
 }
