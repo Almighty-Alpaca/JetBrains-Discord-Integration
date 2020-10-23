d2fd439bac548941ce50fe89da02529462a07e1f
Almighty Alpaca <aljoschagrebe@gmail.com>
2020-10-23 21:37:44 +0200

    Fix license header

--- buildSrc/src/main/kotlin/org/glavo/javah/JniHeaderGenerator.kt
+++ buildSrc/src/main/kotlin/org/glavo/javah/JniHeaderGenerator.kt
@@ -1,34 +1,4 @@
-/*
- * Copyright 2017-2020 Aljoscha Grebe
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.glavo.javah/*
- * Copyright 2017-2020 Aljoscha Grebe
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package org.glavo.javah
 
 import kotlinx.metadata.KmFunction
 import kotlinx.metadata.jvm.KotlinClassHeader
@@ -46,7 +16,7 @@ import java.lang.System.err
 import java.nio.file.Files
 import java.nio.file.Path
 
-class JniHeaderGenerator constructor(
+class JniHeaderGenerator(
     private val outputDir: Path,
     private val searchPaths: Set<SearchPath> = setOf(RuntimeSearchPath)
 ) {
